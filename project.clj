4add14efdada6e3ffd85e7408b1b5a54a6b68d76
Jeff Rose <rosejn@gmail.com>
2010-04-09 14:00:01 +0200

    upping version for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/osc-clj "0.1.2-SNAPSHOT"
+(defproject overtone/osc-clj "0.1.3-SNAPSHOT"
   :description "An Open Sound Control library for Clojure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
