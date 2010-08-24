9b8ce66742868d866713c4ecbfd76c04f83b56ac
Sam Aaron <samaaron@gmail.com>
2010-08-24 10:56:00 +0200

    update dependencies to Clojure 1.2.0 stable

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject overtone/osc-clj "0.2.0-SNAPSHOT"
   :description "An Open Sound Control library for Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]])
