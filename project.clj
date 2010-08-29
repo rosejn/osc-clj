98222a2164d30d4221094a61bd1da1ec2efb7fad
Jeff Rose <rosejn@gmail.com>
2010-08-29 15:38:09 +0200

    updating version number to make sure maven pulls the latest changes.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/osc-clj "0.2.0-SNAPSHOT"
+(defproject overtone/osc-clj "0.2.1-SNAPSHOT"
   :description "An Open Sound Control library for Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
