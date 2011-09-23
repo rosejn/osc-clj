4e4ad20ece3c03973cc4cb83901a689e20413610
Sam Aaron <samaaron@gmail.com>
2011-09-24 00:04:22 +0200

    Depend on Clojure 1.3.0 final

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject overtone/osc-clj "0.7.0"
+(defproject overtone/osc-clj "0.7.1"
   :description "An Open Sound Control library for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.technomancy/jmdns "3.2.1"]
                  [commons-net "3.0.1"]
-                 [overtone/at-at "0.2.0"]])
+                 [overtone/at-at "0.2.1"]])
